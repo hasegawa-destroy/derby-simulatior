ce1913760a0f4b660e4a3c1b65ee338b19304550
hasegawa <hasegawaj2003@gmail.com>
2026-05-06 00:38:25 +0900

    feature/ヘッダー作成

---
+++ src/app/components/header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="bg-primary p-4 flex flex-col">
+            <p className="text-secondary text-2xl font-bold">WIN</p>
+            <p className="text-secondary text-sm">DerbySimulatior</p>
+        </header>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./src/app/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#111827",
+                secondary: "#eeeeee",
+                tertiary: "#22C55E",
+            },
+        },
+    },
+};
