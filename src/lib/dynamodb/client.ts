c004ed4dc1720b0723fc12faecfb00dc72923483
hasegawa <hasegawaj2003@gmail.com>
2026-05-30 13:42:12 +0900

    feature/DynamoDB呼び出しの認証をIAMに任せる

--- src/lib/dynamodb/client.ts
+++ src/lib/dynamodb/client.ts
@@ -2,11 +2,7 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({
-    region: process.env.DDB_REGION,
-    credentials: {
-        accessKeyId: process.env.DDB_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.DDB_SECRET_ACCESS_KEY!,
-    },
+    region: "ap-northeast-1",
 });
 
 export const db = DynamoDBDocumentClient.from(client);
