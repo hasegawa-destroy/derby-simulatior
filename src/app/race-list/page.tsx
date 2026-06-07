e2b426711fbc96685f8c7619326e1d217754a9b9
hasegawa <hasegawaj2003@gmail.com>
2026-06-08 01:32:36 +0900

    feature/レース一覧画面に所持ポイント表示

--- src/app/race-list/page.tsx
+++ src/app/race-list/page.tsx
@@ -3,8 +3,10 @@
 import { useEffect, useState } from "react";
 import { RaceCard } from "./components/RaceCard"
 import { Race } from "@/types/race";
+import { User } from "@/types/user";
 
 export default function RaceListPage() {
+    const [user, setUser] = useState<User | null>(null);
 
     // レース情報取得
     const [races, setRaces] = useState<Race[] | null>([]);
@@ -19,18 +21,41 @@ export default function RaceListPage() {
         fetchRace();
     }, []);
 
+    // ユーザー情報取得
+    useEffect(() => {
+        async function fetchUser() {
+            const res = await fetch(`/api/user`);
+            const json = await res.json();
+
+            setUser(json);
+        }
+
+        fetchUser();
+    }, []);
+
     if (races == null) {
         return <div>レース読み込み中...</div>
     }
 
     return (
-        <div className="mx-full p-4">
-            <p className="text-2xl font-bold text-center mb-6">開催中のレース</p>
+        <div>
+            {/* ポイント残高 */}
+            <div className='bg-[#3E3F43] px-4 py-6 border-t-2 border-gray-500'>
+                <div className='flex justify-between items-center'>
+                    <p className='text-secondary'>ポイント残高</p>
+                    <p className='text-secondary'>{user?.Point ?? 0} pt</p>
+                </div>
+            </div>
 
-            <div className="flex h-full flex-col gap-4">
-                {races.map((race) => (
-                    <RaceCard key={`${race.PK}#${race.SK}`} race={race} />
-                ))}
+            <div className="mx-full p-4">
+
+                <p className="text-2xl font-bold text-center mb-6">開催中のレース</p>
+
+                <div className="flex h-full flex-col gap-4">
+                    {races.map((race) => (
+                        <RaceCard key={`${race.PK}#${race.SK}`} race={race} />
+                    ))}
+                </div>
             </div>
         </div>
     )
