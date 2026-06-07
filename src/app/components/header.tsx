153653ba1b046aaab61b8dd8426cbf44506e1cb8
hasegawa <hasegawaj2003@gmail.com>
2026-06-07 16:34:53 +0900

    feature/ヘッダーのロゴ押下でレース一覧に遷移

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,8 +1,21 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Header() {
+    const router = useRouter();
+
+    // ロゴ押下処理
+    const handleClick = () => {
+        router.push(`/race-list`);
+    }
+
     return (
-        <header className="bg-primary p-4 flex flex-col">
-            <p className="text-secondary text-2xl font-bold">WIN</p>
-            <p className="text-secondary text-sm">DerbySimulatior</p>
+        <header className="bg-primary p-4">
+            <button className="flex flex-col" onClick={handleClick}>
+                <p className="text-secondary text-2xl font-bold">WIN</p>
+                <p className="text-secondary text-sm">DerbySimulatior</p>
+            </button>
         </header>
     );
 }
